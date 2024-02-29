2c5e5699ee951a71a94a282c9c2475f69e204c65
MacBook Pro <macbookpro@Milos-MacBook.local>
2024-02-29 21:38:44 +0100

    first part of a function completed

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
+const secretNumber = document.querySelector('.number');
+secretNumber.textContent = Math.floor(Math.random() * 20) + 1;
+const guessInput = document.querySelector('.guess');
+
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click' , function() {
+  
+  const message = document.querySelector('.message');
+  let score = document.querySelector('.score');
+  
+  
+
+})
 

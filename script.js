523af3d9c699d2ffffcec9033de2dfb9cebef3f4
MacBook Pro <macbookpro@Milos-MacBook.local>
2024-03-01 09:23:12 +0100

    second update

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 'use strict';
-const secretNumber = document.querySelector('.number');
-secretNumber.textContent = Math.floor(Math.random() * 20) + 1;
-const guessInput = document.querySelector('.guess');
-
+let secretNumber = document.querySelector('.number').textContent = Math.floor(Math.random() * 20) + 1;
+let score = 20
 
 const checkButton = document.querySelector('.check');
 checkButton.addEventListener('click' , function() {
   
+  const guessInput = Number(document.querySelector('.guess').value);
   const message = document.querySelector('.message');
-  let score = document.querySelector('.score');
+  const mainScore = document.querySelector('.score');
   
-  
+
+  if(!guessInput) {
+    message.textContent = "Unknown number!";
+  } else if (guessInput > secretNumber) {
+      if (score > 1) {
+        score--;
+        mainScore.textContent = score;
+        message.textContent = "Too high!";
+      } else {
+        message.textContent = "Game over!"
+        mainScore.textContent = 0;
+      }
+
+  } else if (guessInput < secretNumber) {
+
+    if (score > 1) {
+      score--;
+      mainScore.textContent = score;
+      message.textContent = "Too low!"; 
+    } else {
+      message.textContent = "Game over!";
+      mainScore.textContent = 0;
+    }
+  }
+
+
+
 
 })
 
